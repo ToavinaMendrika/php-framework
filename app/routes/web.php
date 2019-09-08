5458d8b7286db51d60db4fc20e4891d9de5e0953
ToavinaMendrika <mendrika.toavina.andriantsivolosy@esti.mg>
2019-09-09 01:00:43 +0300

    framework: add request interface routes cb

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
 
 $router->get('/blog','BlogController@index');
- $router->get('/article',function($request){
+
+ $router->get('/article',function(ServerRequestInterface $request){
      return new Response(200, [], '<h1>Bienvenue sur le article</h1>');
  });
- $router->get('/article/{id}',function($request,$id){
+ $router->get('/article/{id}',function(ServerRequestInterface $request,int $id){
      return new Response(200, [], '<h1>Bienvenue sur le article'.$id.'</h1>');
  });
