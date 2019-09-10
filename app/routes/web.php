de8cce98bd47876d80cbb6c6d1c158a2f9ffcce7
ToavinaMendrika <mendrika.toavina.andriantsivolosy@esti.mg>
2019-09-10 13:12:37 +0300

    Framework API

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,13 +1,34 @@
 <?php
 
 use GuzzleHttp\Psr7\Response;
-use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 $router->get('/blog','BlogController@index');
+$router->get('/blog/{id}/{slug}','BlogController@show');
 
- $router->get('/article',function(ServerRequestInterface $request){
-     return new Response(200, [], '<h1>Bienvenue sur le article</h1>');
- });
- $router->get('/article/{id}',function(ServerRequestInterface $request,int $id){
-     return new Response(200, [], '<h1>Bienvenue sur le article'.$id.'</h1>');
- });
+$router->before('GET|POST', '/blog/.*', function($request) {
+    if(isset($request->getHeader('Authorization')[0])){
+        if($request->getHeader('Authorization')[0] === 'key'){
+            return new Response();          
+        }
+        else {
+            
+        }
+    }
+    else {     
+        exit('Unothorized resource');
+    }
+
+});
+
+$router->post('/blog/create','BlogController@create');
+
+$router->get('/article',function(Request $request){
+    return new Response(200, [], '<h1>Bienvenue sur le article</h1>');
+});
+$router->get('/article/{id}',function(Request $request,int $id){
+    return new Response(200, [], '<h1>Bienvenue sur le article'.$id.'</h1>');
+});
+$router->set404(function() {
+    return new Response(404, [], '<h1>Page Not found</h1>');
+});
