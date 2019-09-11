8a8723b314764b2b9f305f96778987456442b361
DavidRambolajaona <david.rambolajaona@esti.mg>
2019-09-11 14:36:01 +0300

    Ajout configuration 404

--- app/routes/web.php
+++ app/routes/web.php
@@ -4,5 +4,10 @@ use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 $router->get('/blog','BlogController@index');
+
+$router->set404(function() {
+    return new Response(404, [], '<h1>Page Not found</h1>');
+});
+
 $router->post('/user/register', 'UserController@register');
 $router->post('/user/login', 'UserController@login');
