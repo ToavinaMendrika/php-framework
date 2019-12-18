03d9806e409f604c43f81fa72440642d0818a57f
ToavinaMendrika <mendrika.toavina.andriantsivolosy@esti.mg>
2019-12-18 12:49:43 +0300

    Detect connected user on Websocket

--- nodejs/server.js
+++ nodejs/server.js
@@ -10,15 +10,35 @@ let server = app.listen(3000, _ =>{
 })
 
 const io = socket(server)
+
+let connectedUsers = []
+
+
+
 io.on('connection', (socket) => {
     console.log('Un utilisateur est connecter')
 
     socket.on('disconnect', function(){
-        console.log('user disconnected')
+        console.log('user disconnected: ')
     })
 
     socket.on('user_connection', (user) => {
         console.log('user connection: ' + user.pseudo)
-        io.emit('user_connection', user)
+        connectedUsers.push(user)
+        
+        io.emit('userConnected', connectedUsers)
+    })
+
+    socket.on('new_message', message =>{
+        io.emit('userConnected', connectedUsers)
+    })
+
+    socket.on('user_logout', (user) => {
+        connectedUsers = connectedUsers.filter(function(connectedUser,){
+            return connectedUser.id != user.id
+        });
+        io.emit('userConnected', connectedUsers)
     })
 })
+
+app.get('/user/connected', (req, res) => res.send(connectedUsers))
